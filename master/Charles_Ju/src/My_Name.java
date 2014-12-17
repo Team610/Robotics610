b1699b3614910a3e9937af7ad1207b451ac3d692
Charles Ju <charlesju@crescentschool.org>
2014-12-17 15:00:46 -0500

    Hello World Test

---
+++ master/Charles_Ju/src/My_Name.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Charles
+ *
+ */
+public class My_Name {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
